e8340f6242a0487845803710120145bd0285b937
juli_e <julia.ero@euedge.com>
2019-03-25 13:42:46 +0100

    manifest path

--- src/main.js
+++ src/main.js
@@ -12,7 +12,7 @@ Vue.config.productionTip = false
 const prod = process.env.NODE_ENV === 'production'
 const shouldSW = 'serviceWorker' in navigator && prod
 if (shouldSW) {
-  navigator.serviceWorker.register('/service-worker.js').then(() => {
+  navigator.serviceWorker.register('./service-worker.js').then(() => {
     console.log("Service Worker Registered!")
   })
 }
