b2cde27078db49f942a5e5ad880f0646065647e1
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-03 12:08:17 +0000

    Deploy DecentLabs/lightproject to github.com/DecentLabs/lightproject.git:gh-pages

---
+++ precache-manifest.7de5e627af967e54aa407827b11e2aeb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "66b6c8270462b122d25a",
+    "url": "js/chunk-vendors.afa12ce5.js"
+  },
+  {
+    "revision": "bffbcc0ca2ce1d53c6a8",
+    "url": "js/app.6f696cf4.js"
+  },
+  {
+    "revision": "7a3f1f62be0d26ad011e2c799c866abe",
+    "url": "index.html"
+  },
+  {
+    "revision": "66b6c8270462b122d25a",
+    "url": "css/chunk-vendors.771522da.css"
+  },
+  {
+    "revision": "bffbcc0ca2ce1d53c6a8",
+    "url": "css/app.05b195d5.css"
+  }
+];
